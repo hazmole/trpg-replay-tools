38882dae8c8f4cc4946216fb93d9760c1fa407f1
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2023-06-29 17:51:20 +0800

    [init] dev...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trpg-replay-tools';
+  title = '哈絲的團錄工具';
+  version = "0.01";
+  navbarArr = [
+    { title: '編輯器', url: 'editor' },
+    { title: '播放器', url: 'player' },
+  ];
 }
