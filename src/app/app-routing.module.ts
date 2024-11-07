25e8acc0c11c2d018eaf003030ac4bacaf764748
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2024-11-07 17:41:11 +0800

    fix title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { WelcomeComponent } from './views/home/welcome/welcome.component';
 import { EditorComponent } from './views/home/editor/editor.component';
 import { PlayerComponent } from './views/home/player/player.component';
 
+const webTitle_prefix = "哈絲的團錄工具 - ";
+
 const routes: Routes = [
-  { path: 'editor', component: EditorComponent, title: '編輯器' },
-  { path: 'player', component: PlayerComponent, title: '播放器' },
-  { path: '', component: WelcomeComponent, title: '首頁' },
+  { path: 'editor', component: EditorComponent, title: webTitle_prefix+'編輯器' },
+  { path: 'player', component: PlayerComponent, title: webTitle_prefix+'播放器' },
+  { path: '', component: WelcomeComponent, title: webTitle_prefix+'首頁' },
   { path: '**', redirectTo: '' }
 ];
 
